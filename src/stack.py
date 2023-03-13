1887661217c9c0e7a97f62c2bb764d8b9a2cd97b
user <iiiskhakov1990@gmail.com>
2023-03-14 01:30:49 +0300

    Написание стека и тестов к нему.

--- src/stack.py
+++ src/stack.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,7 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
 
     def push(self, data):
         """
@@ -23,12 +24,13 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        node = Node(data, self.top)
+        self.top = node
 
-    def pop(self):
-        """
-        Метод для удаления элемента с вершины стека и его возвращения
-
-        :return: данные удаленного элемента
-        """
-        pass
+    # def pop(self):
+    #     """
+    #     Метод для удаления элемента с вершины стека и его возвращения
+    #
+    #     :return: данные удаленного элемента
+    #     """
+    #     pass
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,36 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+
+import pytest
+from src.stack import Node, Stack
+
+@pytest.fixture
+def n1():
+    return Node(100, None)
+
+@pytest.fixture
+def n2(n1):
+    return Node(200, n1)
+
+@pytest.fixture
+def stack_1():
+    return Stack()
+
+def test_node_init (n1,n2):
+    assert n2.data == 200
+    assert n1.data == 100
+    assert n2.next_node == n1
+    assert n1.next_node == None
+
+def test_stack_init(stack_1):
+    assert stack_1.top == None
+
+def test_stack_push(stack_1):
+    stack_1.push(300)
+    stack_1.push(400)
+
+    assert stack_1.top.data == 400
+    assert stack_1.top.next_node.data == 300
+
+
+
+
